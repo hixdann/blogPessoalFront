c46fe2710ac2922fa961157907ec0b0168c9d77b
hixdann <danilo.f.almeida@outlook.com>
2025-02-13 13:45:38 -0300

    Adicionado Biblioteca Axios, Interfaces, Service, Lógica do Cadastro de Usuário e AuthContext

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,20 +1,34 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+
+    const navigate = useNavigate();
+
+    const { handleLogout } = useContext(AuthContext)
+
+    function logout() {
+
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/')
+    }
+    
     return (
         <>
-            <div className='w-full flex justify-center py-4
-            			   bg-[#750012] text-white'>
-            
+            <div className='w-full bg-[#750012] text-white
+                flex justify-center py-4'>
+
                 <div className="container flex justify-between text-lg px-4">
-                <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
                     <div className='flex gap-4'>
-                        <span>Postagens</span>
+                    <span>Postagens</span>
                         <span>Temas</span>
                         <span>Cadastrar tema</span>
                         <span>Perfil</span>
-                        <span>Sair</span>
+                        <span><Link to='' onClick={logout} className='hover:underline'>Sair</Link></span>
                     </div>
                 </div>
             </div>
